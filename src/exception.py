e8da9a07e88b49b5f43fac22867f375e1dc86bca
MUHAMMED SUNAIN@LAPTOP <msaban371@gmail.com>
2024-08-21 22:49:43 +0530

    commit

--- src/exception.py
+++ src/exception.py
@@ -1,36 +1,18 @@
 import sys
-from src.logger import logging  # Ensure that the logger module is correctly imported
-
-def error_message_detail(error, error_detail: sys):
-    """
-    Extracts detailed error information.
-    
-    :param error: The error message.
-    :param error_detail: The sys module to extract detailed traceback information.
-    :return: Formatted error message string.
-    """
-    _, _, exc_tb = error_detail.exc_info()
-    file_name = exc_tb.tb_frame.f_code.co_filename
-    error_message = "Error occurred in python script name [{0}] line number [{1}] error message[{2}]".format(
-        file_name, exc_tb.tb_lineno, str(error)
-    )
-    return error_message
+import logging
 
 class CustomException(Exception):
-    """
-    Custom Exception class to capture and log detailed error messages.
-    """
     def __init__(self, error_message, error_detail: sys):
         super().__init__(error_message)
-        self.error_message = error_message_detail(error_message, error_detail=error_detail)
-    
+        self.error_message = CustomException.get_detailed_error_message(error_message, error_detail)
+
+    @staticmethod
+    def get_detailed_error_message(error_message, error_detail: sys):
+        _, _, exc_tb = error_detail.exc_info()
+        file_name = exc_tb.tb_frame.f_code.co_filename
+        line_number = exc_tb.tb_lineno
+        detailed_message = f"Error occurred in script: [{file_name}] at line number: [{line_number}] error message: [{error_message}]"
+        return detailed_message
+
     def __str__(self):
         return self.error_message
-
-# Example usage of CustomException
-try:
-    # Some code that may raise an exception
-    raise ValueError("This is a sample error")
-except ValueError as e:
-    # Log the error or handle it as needed
-    logging.error(CustomException(str(e), sys))
